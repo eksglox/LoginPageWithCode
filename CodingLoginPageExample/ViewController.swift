0203d3d16176c4d83f95b096dc18171c3f1e2dbe
Mehmet Nuri Akyol <eksglox@yandex.com>
2020-12-16 15:06:12 +0300

    Initial Commit

---
+++ CodingLoginPageExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingLoginPageExample
+//
+//  Created by mehmet  akyol on 16.12.2020.
+//  Copyright © 2020 mehmet  akyol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
